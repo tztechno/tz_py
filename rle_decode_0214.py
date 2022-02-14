ff473b6586e2c25bd43665a5d7eb468f5e3280a9
tztechno <47345279+tztechno@users.noreply.github.com>
2022-02-14 22:24:22 +0900

    Add files via upload

---
+++ rle_decode_0214.py
@@ -0,0 +1,16 @@
+def rle_decode(mask_rle, shape, color=1):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (height,width) of array to return 
+    Returns numpy array, 1 - mask, 0 - background
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0] * shape[1], dtype=np.float32)
+    
+    for lo, hi in zip(starts, ends):
+        img[lo : hi] = color
+        
+    return img.reshape(shape)
